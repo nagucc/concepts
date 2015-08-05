fa216182126c76046ee42ee0bf17bff553ccd3ed
Ynu AppEngine <yae@ynu.edu.cn>
2015-08-05 18:04:37 +0800

    names & descs functions complete

--- test/concept-manager.js
+++ test/concept-manager.js
@@ -5,7 +5,7 @@ var should = require("should");
 var neo = require('neo4j');
 var ConceptManager = require('../lib/concept-manager');
 
-var url = process.env.NEO_HOST || 'http://localhost:7474';
+var url = process.env.NEO_HOST || 'http://neo4j.ynu.edu.cn';
 var db = new neo.GraphDatabase({url:url});
 
 var cm = new ConceptManager(db);
@@ -26,19 +26,43 @@ describe('Concept model test', function () {
 
 	it('get.获取Concept', function (done) {
 		cm.get(concept_id, function (err, concept) {
-			should.not.exist(err);
 			concept.id.should.above(0);
 			done();
 		});
 	});
 	
-	it('names.获取Concept的names', function(done){
-		con.names(function(err, names){
-			should.not.exists(err);
-			should.exists(names);
-			done();
+	it('addName.添加Concept的name', function(done){
+		con.addName('testName', function(err, result){
+			con.names(function(err, names){
+				names.length.should.above(0);
+				names[0].should.eql('testName');
+				done();
+			});
 		});
 	});
+	
+	it('addDesc', function(done){
+		con.addDesc('testDesc', function(err, result){
+			con.descriptions(function(err, values){
+				values.length.should.above(0);
+				values[0].should.eql('testDesc');
+				done();
+			});
+		});
+	});
+	
+	it('removeName.删除Concept的name', function(done){
+		con.removeName('testName', function(err, result){
+			should.not.exist(err);
+			con.names(function(err, names){
+				names.length.should.eql(0);
+				done();
+			});
+		});
+	});
+	
+	
+	
 	
 	it('del.删除Concept', function (done) {
 		cm.del(concept_id, function (err, result) {
